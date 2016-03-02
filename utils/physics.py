fc3246d5b657a52882c69cfe9732ec95ced84448
Sidharth <sidharth01g@gmail.com>
2016-03-02 19:47:48 +0000

    Plot current and theta

--- utils/physics.py
+++ utils/physics.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 
 force_integral = 0
 pi = math.pi
+interval = 1
 
-def current(theta, theta_minus, kp, kd, ki):
+def current(theta, theta_minus, kp, ki, kd):
     k_theta_to_force = 10
     force = k_theta_to_force * math.tan(theta)
     force_minus = k_theta_to_force * math.tan(theta_minus)
@@ -20,13 +21,13 @@ def proportional(force):
 
 
 def differential(force, force_minus):
-    interval = 1
+    global interval
     d = (force - force_minus)/interval
     return d
 
 
 def integral(force, force_minus):
-    interval = 1
+    global interval
     force_threshold_integral = 20
     if (force > -force_threshold_integral and
          force < force_threshold_integral):
@@ -35,30 +36,38 @@ def integral(force, force_minus):
     return i
 
 
-def test():
+def test_sinusoidal_theta():
     global pi
+    
+    kp = 0
+    ki = 0
+    kd =10
+    
     theta_initial = 0
-    theta =1
     theta_minus = theta_initial
+    theta =1
     theta_array = []
     current_array = []
-    i = 0
+
     for angle in numpy.arange(-pi/2, pi/2, 0.1):
         theta = math.sin(angle)
         print('Theta: ' + str(theta*180/pi))
-        curr = current(theta, theta_minus, 1, 1, 1)
-        print('Current: ' + str(curr))
-        print
+        curr = current(theta, theta_minus, kp, ki, kd)
+        print('Current: ' + str(curr) + '\n')
+
         theta_minus = theta
         theta_array.append(theta)
         current_array.append(curr)
-        i = i + 1
-    print theta_array
-    print current_array
-    plt.subplot(1)
+
+    print len(theta_array)
+    print len(current_array)
+    plt.subplot(211)
     plt.plot(theta_array)
-    plt.subplot(2)
+    plt.subplot(212)
     plt.plot( current_array)
+    # plt.axis('equal')
+    plt.show()
+
 
 if __name__ == '__main__':
-    test()
+    test_sinusoidal_theta()
