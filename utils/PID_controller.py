878121d1e03bc5dec986b12fe48116d6988c173b
Sidharth <sidharth01g@gmail.com>
2016-03-11 16:06:01 +0000

    Added tangent control

--- utils/PID_controller.py
+++ utils/PID_controller.py
@@ -44,35 +44,51 @@ def filter_PID(x_n, alpha, kp, ki, kd):
     return y[0]
 
 
+def limit(x, lim):
+    if x > 0:
+        return min(x, lim)
+    else:
+        return max(x, -lim)
+
+
+def limited_tan(theta, tan_limit):
+    temp = limit(math.tan(theta), tan_limit)
+    return temp
+
+
 def main():
     global pi, Ts
     
-    theta_array = []
+    theta_err_array = []
     y_array = []
 
     set_Ts(0.01)
     t_min = 0
     t_max = 1
-    f = 5 # frequency of theta in Hertz
+    f = 5 # frequency of theta_err in Hertz
     alpha = -0
-    kp = 1
-    ki = 1
-    kd = 0
+    kp = 0
+    ki = 0
+    kd = 1
+    beta_1 = 1
+    beta_2 = 1
+    tan_limit = 10
 
     for time in numpy.arange(t_min, t_max, Ts):
-        theta = math.sin(2*pi*f*time)
-        y_n =  filter_PID(theta, alpha, kp, ki, kd)
+        theta_err = (pi/2)*math.sin(2*pi*f*time)
+        y_n =  limit((beta_1 * filter_PID(theta_err, alpha, kp, ki, kd) +
+            beta_2 * limited_tan(theta_err, tan_limit)), 6000)
         print y
-        theta_minus = theta
-        theta_array.append(theta)
+        theta_err_minus = theta_err
+        theta_err_array.append(theta_err)
         y_array.append(y_n)
 
-    print len(theta_array)
+    print len(theta_err_array)
     print len(y_array)
-    print (theta_array)
+    print (theta_err_array)
     print (y_array)
     plt.subplot(211)
-    plt.plot(theta_array)
+    plt.plot(theta_err_array)
     plt.subplot(212)
     plt.plot( y_array)
     # plt.axis('equal')
